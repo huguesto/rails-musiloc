a8cc04cfc90aa8cda6c34c83daa6444c6b34e1d7
Samia Dahmouni <dsamia@live.fr>
2017-11-14 16:53:23 +0100

    created reviews_controller to update booking

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,26 @@
+class ReviewsController < ApplicationController
+
+  before_action :set_review, only: [:edit]
+
+  def edit
+  end
+
+  def update
+    @booking.update(booking_params)
+    if @booking.save
+      redirect_to bookings_path(@booking)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:review, :rating)
+  end
+end
