e9c279a584749a3826b14e29e90a1ca292641fd1
Samia Dahmouni <dsamia@live.fr>
2017-11-14 12:01:50 +0100

    update categories in instrument model

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -1,8 +1,10 @@
 class Instrument < ApplicationRecord
+  CATEGORIES = ["Vent", "Corde", "Percussion", "Electronique"]
+
   belongs_to :user
   has_many :bookings
 
   validates :name, presence: true
-  validates :category, presence: true
+  validates :category, inclusion: { in: CATEGORIES }
   validates :hourly_price, presence: true
 end
